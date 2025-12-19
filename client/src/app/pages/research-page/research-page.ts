dcb1a8cf026277987de280419e1099a83f624f98
Owen Holm <owenholm87@gmail.com>
2025-12-19 16:11:04 -0500

    Page creation pt. 1
    -Created pages for Home and Research

---
+++ client/src/app/pages/research-page/research-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-research-page',
+  imports: [],
+  templateUrl: './research-page.html',
+  styleUrl: './research-page.css',
+})
+export class ResearchPage {
+
+}
