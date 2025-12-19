dba49c9b80f5592946e3c734f22f91f38e10ce72
Owen Holm <owenholm87@gmail.com>
2025-12-19 16:11:28 -0500

    Set up routing
    - Set up routing for home and research pages

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { ResearchPage } from './pages/research-page/research-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomePage,
+    },
+    {
+        path: 'research',
+        component: ResearchPage,
+    }
+];
